1782147551ff7b5c4493c985b5377acae89b56e5
phrz <phrz@me.com>
2016-03-18 09:45:09 -0500

    Added example Config class (generic)

---
+++ class/Config.example.php
@@ -0,0 +1,19 @@
+<?php
+    class Config {
+
+        public static $database = [
+            'host' => '',
+            'username' => '',
+            'password' => '',
+            'database' => '',
+            'charset' => 'utfmb4', // table charset
+            'prefix' => '', // table prefix
+            'timezone' => 'America/Chicago' // Central Time with DST
+        ];
+
+        public static $nonce = [
+			       // Nonce expiry interval:
+            'expiry' => 'PT5M' // DateInterval spec (5 Minutes)
+        ];
+    }
+?>
